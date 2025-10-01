a49631d4b90f26f21cdf3073b9b75e0439d40294
Sofia383048 <a383048@uach.mx>
2025-10-01 08:18:16 -0700

    Create programa1.py

---
+++ programa1.py
@@ -0,0 +1,6 @@
+# ESte es un script de python de prueba para el repositorio
+def saludo(nombre):
+  return "Hola " +nombre
+
+# se invoca la funcion saludo para saludar a alguien llamada sofia 
+saludo("Sofia")
